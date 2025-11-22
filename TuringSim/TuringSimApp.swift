6db06bf502efa8924421a416d7c3d576b04ad698
Steve Roe <steveroe@roedesigns.dev>
2025-11-22 12:54:11 -0800

    Initial Commit

---
+++ TuringSim/TuringSimApp.swift
@@ -0,0 +1,17 @@
+//
+//  TuringSimApp.swift
+//  TuringSim
+//
+//  Created by Steve Roe on 11/22/25.
+//
+
+import SwiftUI
+
+@main
+struct TuringSimApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TuringSimTests/TuringSimTests.swift
@@ -0,0 +1,17 @@
+//
+//  TuringSimTests.swift
+//  TuringSimTests
+//
+//  Created by Steve Roe on 11/22/25.
+//
+
+import Testing
+@testable import TuringSim
+
+struct TuringSimTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
