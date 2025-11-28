a632a5a7c01dd2a54a384d8a4593669003cd0dcc
Steve Roe <steveroe@roedesigns.dev>
2025-11-28 08:18:59 -0800

    Moved simulation code into global actor.
    The actor is controlled from a task launched in the models init().

---
+++ Core/SimSnapshot.swift
@@ -0,0 +1,8 @@
+//
+//  SimSnapshot.swift
+//  TuringSim
+//
+//  Created by Steve Roe on 11/25/25.
+//
+
+import Foundation
---
+++ TuringSim/SimActor.swift
@@ -0,0 +1,8 @@
+//
+//  SimActor.swift
+//  TuringSim
+//
+//  Created by Steve Roe on 11/25/25.
+//
+
+import Foundation
