96e7a15c49e985c037dde46ccef13dd16da539d2
Steve Roe <steveroe@roedesigns.dev>
2025-12-25 11:23:57 -0800

    Added Turing Map images.

---
+++ Views/TuringMapView.swift
@@ -0,0 +1,18 @@
+//
+//  TuringMapView.swift
+//  TuringSim
+//
+//  Created by Steve Roe on 12/22/25.
+//
+
+import SwiftUI
+
+struct TuringMapView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TuringMapView()
+}
