fb06b9141387719392565293a7302f0e9d3d7f1c
Steve Roe <steveroe@gmail.com>
2026-05-01 14:34:09 -0700

    Switch to current ignore safe area method

--- TuringSimIos/ContentView.swift
+++ TuringSimIos/ContentView.swift
@@ -11,7 +11,7 @@ struct ContentView: View {
 
     var body: some View {
         ZStack {
-            Color(red: 0.6, green: 0.3, blue: 0.2).edgesIgnoringSafeArea(.all)
+            Color(red: 0.6, green: 0.3, blue: 0.2).ignoresSafeArea()
             GeometryReader { geometry in
                 CoreView(viewTypeA: .TILE, viewTypeB: .NONE).frame(width: geometry.size.width,
                                  height: geometry.size.height)
