2d36cb294f8f4f5c1fe0402ea8497f52122afff2
Steve Roe <steveroe@gmail.com>
2026-05-01 09:31:02 -0700

    Do not run simulation engine when paused
    Refactored code that runs simulation engine to reduce method size.
    Added MARK comments.

--- Core/SimModel.swift
+++ Core/SimModel.swift
@@ -36,32 +36,31 @@ final class SimModel {
     private var B: [Double] = []
     private var simTask: Task<Void, Error>?
 
+    // MARK: - Simulation Engine
+    
     init() {
         frameTimes = Array(repeating: 0.0, count: frameSamples)
         frameGenerations = Array(repeating: 0, count: frameSamples)
         simTask = Task {
             let engine = await SimEngine()
-            f = await engine.f
-            k = await engine.k
+            await loadSimParameters(from: engine)
             while true {
+                await processSimParameterChanges(for: engine)
+                let simCellsModified = await processRequestFlags(for: engine)
                 isSimRunning = await engine.isRunning()
-                if !isSimRunning {
+                if !isSimRunning && !simCellsModified {
                     try await Task.sleep(for: .milliseconds(50))
-                }
-                
-                let engine_f = await engine.f
-                let engine_k = await engine.k
-                if f != engine_f || k != engine_k {
-                    f = (1000.0 * f).rounded() / 1000.0
-                    k = (1000.0 * k).rounded() / 1000.0
-                    await engine.set_f(f)
-                    await engine.set_k(k)
+                    continue // Skip snapshot processing.
                 }
 
                 let start = Date.now
 
                 let snapshot = await engine.evolve(for: generationsPerFrame)
-                
+                cellsPerEdge = snapshot.cellsPerEdge
+                A = snapshot.A
+                B = snapshot.B
+
+                // Update average sim generatation rate.
                 if (generation == snapshot.generation) {
                     generationRate = 0.0
                 } else {
@@ -73,27 +72,6 @@ final class SimModel {
                     / frameTimes.reduce(0.0, +)
                     frameIndex = (frameIndex + 1) % frameSamples
                 }
-                
-                cellsPerEdge = snapshot.cellsPerEdge
-                A = snapshot.A
-                B = snapshot.B
-
-                if startRequested {
-                    await engine.start()
-                    startRequested = false
-                }
-                if pauseRequested {
-                    await engine.stop()
-                    pauseRequested = false
-                }
-                if seedRequested {
-                    await engine.seedRandomly()
-                    seedRequested = false
-                }
-                if resetRequested {
-                    await engine.resetGraph()
-                    resetRequested = false
-                }
             }
         }
     }
@@ -101,11 +79,9 @@ final class SimModel {
     deinit {
         simTask?.cancel()
     }
-
-    func abLevel(a: Double, b: Double) -> Double {
-        return (a + b == 0) ? 1.0 : b / (a + b)
-    }
     
+    // MARK: Simulation Engine Observation
+
     func cellLevel(w: Int, h: Int) -> Double {
         let j = w * cellsPerEdge + h
         return abLevel(a: A[j], b: B[j])
@@ -131,6 +107,8 @@ final class SimModel {
     func isRunning() -> Bool {
         return isSimRunning
     }
+    
+    // MARK: Simulation Engine Control
 
     func start() {
         startRequested = true
@@ -146,5 +124,52 @@ final class SimModel {
 
     func reset() {
         resetRequested = true
+    }
+    
+    // MARK: - Private Methods
+    
+    private func processRequestFlags(for engine: SimEngine) async -> Bool {
+        var simCellsModified = false
+        
+        if startRequested {
+            await engine.start()
+            startRequested = false
+        }
+        if pauseRequested {
+            await engine.stop()
+            pauseRequested = false
+        }
+        if seedRequested {
+            await engine.seedRandomly()
+            seedRequested = false
+            simCellsModified = true
+        }
+        if resetRequested {
+            await engine.resetGraph()
+            resetRequested = false
+            simCellsModified = true
+        }
+        
+        return simCellsModified
+    }
+    
+    private func loadSimParameters(from engine: SimEngine) async {
+        f = await engine.f
+        k = await engine.k
+    }
+    
+    private func processSimParameterChanges(for engine: SimEngine) async {
+        let engine_f = await engine.f
+        let engine_k = await engine.k
+        if f != engine_f || k != engine_k {
+            f = (1000.0 * f).rounded() / 1000.0
+            k = (1000.0 * k).rounded() / 1000.0
+            await engine.set_f(f)
+            await engine.set_k(k)
+        }
+    }
+    
+    private func abLevel(a: Double, b: Double) -> Double {
+        return (a + b == 0) ? 1.0 : b / (a + b)
     }
 }
