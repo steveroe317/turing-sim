3ed84b545958d20c942e411f64ba6972b8da8635
Steve Roe <steveroe@gmail.com>
2026-05-02 10:37:31 -0700

    Replace GeometryReader with frame max width/height

--- TuringSimIos/ContentView.swift
+++ TuringSimIos/ContentView.swift
@@ -8,15 +8,12 @@
 import SwiftUI
 
 struct ContentView: View {
-
+    
     var body: some View {
         ZStack {
             Color(red: 0.6, green: 0.3, blue: 0.2).ignoresSafeArea()
-            GeometryReader { geometry in
-                CoreView(viewTypeA: .tile, viewTypeB: .none).frame(width: geometry.size.width,
-                                 height: geometry.size.height)
-            }
-
+            CoreView(viewTypeA: .tile, viewTypeB: .none)
+                .frame(maxWidth: .infinity, maxHeight: .infinity)
         }
         .buttonStyle(SimButtonStyle())
     }
